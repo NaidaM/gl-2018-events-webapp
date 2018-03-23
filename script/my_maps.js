2897a2e69b5a2490f1aa09dd000ea1b8794e1ac3
meftah <meftah@informatique.univ-paris-diderot.fr>
2018-03-23 18:18:08 +0100

    New map modal, beginning of the integration of maps.

--- script/my_maps.js
+++ script/my_maps.js
@@ -2,6 +2,8 @@ var host = "http://localhost";
 var port = "8080";
 var path = host+":"+port+"/api/v1/";
 
+var formNewMap = document.querySelector("#form-newMap");
+
 console.log(localStorage.getItem('access_token'));
 
 var axios = axios.create({
@@ -13,10 +15,51 @@ var axios = axios.create({
 axios.get ("users/"+localStorage.getItem('pseudo')+"/maps") 
 		.then(function (response) {			
 			if (response.status == 200) {
-				console.log(response.data);
-			}
-			console.log(response);
+				if(response.data.maps.length == 0){
+					document.querySelector("#containerMaps").style.display = "none";
+					document.querySelector("#containerWelcome").style.display = "block";
+				}
+				else {
+					document.querySelector("#containerMaps").style.display = "block";
+					document.querySelector("#containerWelcome").style.display = "none";
+				}
+				console.log(response.data);	
+			}			
 		})
 		.catch(function (err) {
-			console.log(err.response.data.message[0]);
+			if (err.response.status == 404) 
+				document.querySelector("#containerMaps").style.display = "none";
 		});
+		
+		
+formNewMap.addEventListener("submit", function(e) {
+	e.preventDefault();
+	var nameMap = document.querySelector("#idNameMap").value;
+	var descMap = document.querySelector("#idDescMap").value;	
+	
+	var error = document.querySelector(".error");	
+	
+	if (nameMap == "") {
+		error.innerHTML = "Ce champ est obligatoire!";
+		error.style.display = "block";
+	}
+	
+	else {
+		axios.post ("users/"+localStorage.getItem('pseudo')+"/maps", {
+			name: nameMap,
+			description: descMap,
+			taglist: "[tag]",
+			visibility: true
+		})
+		.then(function (response) {			
+			if (response.status == 201) {
+				console.log(response.data);
+				document.location.href = "my_maps.html";
+			}
+		})
+		.catch(function (err) {
+		});
+		
+	}
+});
+		
