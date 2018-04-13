445093444010f5a290d2f3c9b8cfbe4de97e4eb4
meftah <meftah@informatique.univ-paris-diderot.fr>
2018-04-13 17:00:39 +0200

    Tags input in search bar, axios integration of the research (no display page yet)

--- script/app.js
+++ script/app.js
@@ -1,3 +1,12 @@
+var host = "http://localhost";
+var port = "8080";
+var path = host+":"+port+"/api/v1/";
+					
+var axios2 = axios.create({
+  baseURL: path,
+  timeout: 5000
+});
+
 var logout = function () {
 
     if (confirm("Are tou sure to logout ?")) {
@@ -10,4 +19,25 @@ var logout = function () {
 
 document.querySelector("#logoutBtn").addEventListener("click",function () {
    logout();
+});
+
+$('#searchBtn').click(function(){
+	var searchTags = $("#searchTags").tagsinput('items');
+	if (searchTags!=null) {
+		var tags = "";
+		for (var  i = 0;i<searchTags.length;i++) {
+			tags = tags+"tag="+searchTags[i]+"&";
+		}
+
+		axios2.get("maps/searchmap?"+tags) 
+			.then(function (response) {		
+				
+				if (response.status == 200) {
+					console.log(response.data);
+				}
+			})
+			.catch(function (err) {
+				  console.log(err);
+			});	
+	}
 });
